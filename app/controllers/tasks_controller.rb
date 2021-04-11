9b79d195f555bc67533b47a9cb3f0c455ec23ae1
Toyoda Kenji <toyoda@wyrd.co.jp>
2021-04-11 15:13:14 +0900

    chore: rubocopの自動修正を実行。指摘対象である必要のないファイルを除外。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
   def index
-    user_id = 1 #ログイン機能実装後、ログインIDを user_id に格納
+    user_id = 1 # ログイン機能実装後、ログインIDを user_id に格納
 
     @tasks = Task.where(user_id: user_id)
   end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Label < ApplicationRecord
   has_many :tasks, through: :label_tasks
   has_many :label_tasks
-  validates :label, presence: true, format: { with: /\A[a-zA-Zぁ-んァ-ン一-龥]+\z/}
+  validates :label, presence: true, format: { with: /\A[a-zA-Zぁ-んァ-ン一-龥]+\z/ }
 end
