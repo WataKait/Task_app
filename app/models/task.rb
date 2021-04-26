9fffc25b4388697b57eb23aa45653381b11664e2
WataKait <watanabe@wyrd.co.jp>
2021-04-26 10:52:43 +0900

    正規表現の撤廃

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,5 @@ class Task < ApplicationRecord
   belongs_to :priority
   belongs_to :status
   belongs_to :label
-  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9ぁ-んァ-ン一-龥_-]+\z/ }
-  validates :description, format: { with: /\A[a-zA-Z0-9ぁ-んァ-ン一-龥_-]+\z/ }
+  validates :name, presence: true
 end
