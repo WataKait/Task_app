1b3556a680a666ed592e00c2e8bb3a70a147afb1
WataKait <watanabe@wyrd.co.jp>
2021-05-06 12:35:37 +0900

    編集機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :set_labels, only: %i[edit update]
+  before_action :set_priorities, only: %i[edit update]
+  before_action :set_statuses, only: %i[edit update]
+
   def index
     # TODO: ログイン機能実装後、user_idを取得してくる
     user_id = 1
@@ -9,5 +13,36 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :user_id, :label_id, :priority_id, :status_id, :time_limit, :description)
+  end
+
+  def set_labels
+    @labels = Label.all
+  end
+
+  def set_priorities
+    @priorities = Priority.all
+  end
+
+  def set_statuses
+    @statuses = Status.all
   end
 end
