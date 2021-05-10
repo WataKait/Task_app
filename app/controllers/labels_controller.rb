7dbe072735b6d34f3d9f55975086bfaaea0e4b51
WataKait <watanabe@wyrd.co.jp>
2021-05-10 10:18:54 +0900

    ラベル作成機能の実装

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -4,4 +4,23 @@ class LabelsController < ApplicationController
   def index
     @labels = Label.all
   end
+
+  def new
+    @label = Label.new
+  end
+
+  def create
+    @label = Label.new(label_params)
+    if @label.save
+      redirect_to labels_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
 end
