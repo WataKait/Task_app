4b6f19d3b5b3944dc5bc7e637e16675856f6a74d
WataKait <watanabe@wyrd.co.jp>
2021-05-10 14:38:57 +0900

    同様の処理のメソッド化

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class LabelsController < ApplicationController
+  before_action :label, only: %i[edit]
+
   def index
     @labels = Label.all
   end
@@ -19,13 +21,10 @@ class LabelsController < ApplicationController
     end
   end
 
-  def edit
-    @label = Label.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @label = Label.find(params[:id])
-    if @label.update(label_params)
+    if label.update(label_params)
       redirect_to labels_path, notice: t('.notice')
     else
       flash.now[:alert] = t('.alert')
@@ -34,6 +33,10 @@ class LabelsController < ApplicationController
   end
 
   private
+
+  def label
+    @label ||= Label.find(params[:id])
+  end
 
   def label_params
     params.require(:label).permit(:name)
