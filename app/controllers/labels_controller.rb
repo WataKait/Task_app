dbb22714a97f669411571bcf1e4342f6915f1751
WataKait <watanabe@wyrd.co.jp>
2021-05-10 11:20:52 +0900

    ラベル編集機能の実装

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -4,4 +4,23 @@ class LabelsController < ApplicationController
   def index
     @labels = Label.all
   end
+
+  def edit
+    @label = Label.find(params[:id])
+  end
+
+  def update
+    @label = Label.find(params[:id])
+    if @label.update(label_params)
+      redirect_to labels_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
 end
