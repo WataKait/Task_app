5c095caaad054d9c92407558b6c287e17181c880
WataKait <watanabe@wyrd.co.jp>
2021-05-10 13:16:08 +0900

    ラベル削除機能の実装

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -4,4 +4,10 @@ class LabelsController < ApplicationController
   def index
     @labels = Label.all
   end
+
+  def destroy
+    label = Label.find(params[:id])
+    label.destroy
+    redirect_to labels_path
+  end
 end
