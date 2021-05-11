95af9cebbff157579211edc332fb2dba173ac554
WataKait <watanabe@wyrd.co.jp>
2021-05-11 11:09:20 +0900

    メソッド化とメソッド呼び出しの修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update]
+  before_action :set_task, only: %i[show edit update destroy]
   before_action :set_labels, only: %i[new create edit update]
   before_action :set_priorities, only: %i[new create edit update]
   before_action :set_statuses, only: %i[new create edit update]
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task.destroy
+    @task.destroy
     redirect_to tasks_path, notice: t('.notice')
   end
 
