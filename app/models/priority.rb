50746df9b9e5a9d8c0bc68cd9acdfab2cbfeb856
WataKait <watanabe@wyrd.co.jp>
2021-05-14 14:55:42 +0900

    priority モデルのバリデーションの修正

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -2,6 +2,6 @@
 
 class Priority < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
-  validates :priority, presence: true, numericality: true, uniqueness: true, length: { maximum: 255 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :priority, presence: true, numericality: true, uniqueness: true
 end
