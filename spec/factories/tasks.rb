4b91b9d2a7355b1c4639a51c3ee4fb488a96fe96
WataKait <watanabe@wyrd.co.jp>
2021-05-14 11:32:49 +0900

    テストデータ定義の修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,7 +4,10 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "task_name#{n}" }
     association :user
+    association :label
     association :priority
     association :status
+    time_limit { Time.zone.now }
+    description { 'description' }
   end
 end
