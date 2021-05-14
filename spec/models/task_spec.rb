fb49556a5d80bd4f63e46c12fb1ec48c59354bc2
WataKait <watanabe@wyrd.co.jp>
2021-05-14 17:49:01 +0900

    テストの追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'name が正常値なら有効' do
+  it 'name, user, priority, status が正常値なら有効' do
     expect(build(:task)).to be_valid
   end
 
@@ -17,5 +17,23 @@ RSpec.describe Task, type: :model do
     task = build(:task, name: SecureRandom.alphanumeric(256))
     task.valid?
     expect(task.errors[:name]).to include(I18n.t('errors.messages.too_long', count: 255))
+  end
+
+  it 'user が無いなら無効' do
+    task = build(:task, user: nil)
+    task.valid?
+    expect(task.errors[:user]).to include(I18n.t('errors.messages.required'))
+  end
+
+  it 'priority が無いなら無効' do
+    task = build(:task, priority: nil)
+    task.valid?
+    expect(task.errors[:priority]).to include(I18n.t('errors.messages.required'))
+  end
+
+  it 'status が無いなら無効' do
+    task = build(:task, status: nil)
+    task.valid?
+    expect(task.errors[:status]).to include(I18n.t('errors.messages.required'))
   end
 end
