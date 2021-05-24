d497460e1d16d9e700ec28b6aa7513c209f4d72f
WataKait <watanabe@wyrd.co.jp>
2021-05-24 16:10:28 +0900

    Revert "終了期限でソートする"
    This reverts commit 72013aab4d7b4535f03389d6e6f25192f50e684f.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
   def index
     # TODO: ログイン機能実装後、user_idを取得してくる
     user_id = 1
-    @tasks = Task.where(user_id: user_id).order(time_limit: :asc)
+    @tasks = Task.where(user_id: user_id)
   end
 
   def show; end
