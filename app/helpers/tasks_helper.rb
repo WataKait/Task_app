9a3f78658a320a28a3f026f1a6c5f43750caf7e1
WataKait <watanabe@wyrd.co.jp>
2021-05-27 11:16:57 +0900

    三項演算子に括弧を付与

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,7 +2,7 @@
 
 module TasksHelper
   def create_sort_link(column, title)
-    direction = column == sort_criteria_column && switch_order == 'asc' ? 'desc' : 'asc'
+    direction = (column == sort_criteria_column && switch_order == 'asc') ? 'desc' : 'asc'
     link_to title, sort: column, direction: direction
   end
 end
