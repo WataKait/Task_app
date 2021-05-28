d42dec2f4d3504caaa4ede190144e7756f6ca8f6
WataKait <watanabe@wyrd.co.jp>
2021-05-28 10:56:46 +0900

    タスクを取得するコードを分けて記述

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,8 @@ class TasksController < ApplicationController
   def index
     # TODO: ログイン機能実装後、user_idを取得してくる
     user_id = 1
-    @tasks = Task.where(user_id: user_id).order("#{sort_criteria_column} #{switch_order}")
+    @tasks = Task.where(user_id: user_id).order("#{sort_criteria_column} desc")
+    @tasks = @tasks.reverse_order if params[:direction] == 'asc'
   end
 
   def show; end
