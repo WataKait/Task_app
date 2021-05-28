31981a7b789c570bddec31cc4bf9667ed9d03d25
WataKait <watanabe@wyrd.co.jp>
2021-05-28 16:31:44 +0900

    三項演算子の変数を変更

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,9 +2,8 @@
 
 module TasksHelper
   def create_sort_link(column, title)
-    sort_criteria_column = set_sort_criteria
-    sort_order = switch_order
-    direction = (column == sort_criteria_column && sort_order == 'asc') ? 'desc' : 'asc'
+    truth_value = (column == set_sort_criteria && switch_order == 'asc')
+    direction = truth_value ? 'desc' : 'asc'
     link_to title, sort: column, direction: direction
   end
 end
