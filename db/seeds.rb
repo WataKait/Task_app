3bcbadae7f7dfbc09b5f188499fb821c701c94b1
WataKait <watanabe@wyrd.co.jp>
2021-06-09 15:36:18 +0900

    シードデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot'
+
+FactoryBot.create(:user)
