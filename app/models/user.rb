5daa685c8503387e447f7a9b998268eaaeb4a91a
WataKait <watanabe@wyrd.co.jp>
2021-06-10 11:35:28 +0900

    バリデーションの付与と検証の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
