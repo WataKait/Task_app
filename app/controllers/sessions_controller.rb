016eb19286bfce750081dfd2872b36d93a52bde1
WataKait <watanabe@wyrd.co.jp>
2021-06-10 13:50:48 +0900

    ログイン機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(name: params[:name].downcase)
+    if user&.authenticate(params[:password])
+      log_in user
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
   end
 end
