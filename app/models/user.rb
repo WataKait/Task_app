1d0751a8455135a70a34fd7c942658201b010a96
WataKait <watanabe@wyrd.co.jp>
2021-06-14 10:54:55 +0900

    パスワードのバリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 8 }
+  has_secure_password validations: false
+  validates :password, presence: true, length: { minimum: 8 }, confirmation: true
 end
