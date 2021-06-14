790183370c1d587968ed747f1b996383cf59328f
WataKait <watanabe@wyrd.co.jp>
2021-06-14 16:32:53 +0900

    ログイン認証の修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(name: params[:name].downcase)
+    user = User.find_by(name: params[:name])
     if user&.authenticate(params[:password])
       log_in user
       redirect_to tasks_path
