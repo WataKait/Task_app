e01629be5ce23e83d9ac91e37b398342a3fc7c55
WataKait <watanabe@wyrd.co.jp>
2021-06-14 12:05:49 +0900

    ユーザ一覧の検証

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,4 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
+  let!(:task) { create(:task, priority: priority, status: status, user: user) }
+  let!(:user) { create(:user, name: 'Taro') }
+
+  let(:priority) { create(:priority, name: 'low', priority: 1) }
+  let(:status) { create(:status, name: 'unstarted') }
+
+  context 'ユーザ一覧' do
+    before do
+      visit users_path
+    end
+
+    it 'hoge' do
+      expect(page).to have_selector '.user_names', text: user.name
+      expect(page).to have_selector '#0', text: user.tasks.size
+    end
+  end
 end
