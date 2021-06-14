cd2d08932a6840426517accd00de22fd83415c3b
WataKait <watanabe@wyrd.co.jp>
2021-06-14 17:48:37 +0900

    rubocop の指摘対応

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,12 +6,11 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def log_out
     session.delete(:user_id)
-    @current_user = nil
   end
 
   def logged_in?
