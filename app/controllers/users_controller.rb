1183705e6218c6751001218903f83b2339a9112f
WataKait <watanabe@wyrd.co.jp>
2021-06-15 16:29:12 +0900

    ユーザ登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all.eager_load(:tasks)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = Task.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
