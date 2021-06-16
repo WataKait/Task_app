b5fa5d755a2b793141afb9f316613e5bfd1d1c4c
WataKait <watanabe@wyrd.co.jp>
2021-06-16 17:57:54 +0900

    ユーザの削除機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all.preload(:tasks)
   end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: t('.notice')
+  end
 end
