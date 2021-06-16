b7ba4ce6ad186be1947fe665d4be41fbada9b031
WataKait <watanabe@wyrd.co.jp>
2021-06-16 11:11:01 +0900

    ユーザ編集機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
   has_secure_password validations: false
-  validates :password, presence: true, length: { minimum: 8, maximum: 255 }, confirmation: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 255 }, confirmation: true, if: :password_was_entered?
+
+  private
+
+  def password_was_entered?
+    password.present? || password_confirmation.present?
+  end
 end
