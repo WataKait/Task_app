bc7ffc46a2a2a141d168c90f3b82442bee138227
WataKait <watanabe@wyrd.co.jp>
2021-06-21 11:43:27 +0900

    admin カラムにデフォルト値を設定

--- db/migrate/20210621022510_add_column_to_user.rb
+++ db/migrate/20210621022510_add_column_to_user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddColumnToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
